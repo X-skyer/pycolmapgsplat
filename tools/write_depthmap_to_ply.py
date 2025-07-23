678f9ee648a4d7a0549900b9b21243b74940f2e9
eric.xc <eric.xc@alibaba-inc.com>
2025-07-23 15:27:48 +0800

    rename

--- tools/write_depthmap_to_ply.py
+++ tools/write_depthmap_to_ply.py
@@ -6,7 +6,7 @@ import numpy as np
 import os
 
 from plyfile import PlyData, PlyElement
-from pycolmap import SceneManager
+from pycolmapgsplat import SceneManager
 from scipy.ndimage.interpolation import zoom
 
 
